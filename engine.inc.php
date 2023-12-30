84c495c09bea89a4311f2792f647cf51fef28e70
cleitonme <cleitonme@gmail.com>
2023-12-30 17:16:17 -0300

    Add files via upload

--- engine.inc.php
+++ engine.inc.php
@@ -1,35 +1,29 @@
 <?php
 require_once('config.inc.php');
+include ("language/$language.php");
 
-function check_socket()
-{ global $f2b;
-  if(!file_exists($f2b['socket'])){ return 'Socket file not found.'; }
-  elseif(!is_readable($f2b['socket'])){ return 'Socket file not readable.'; }
-  elseif(!is_writeable($f2b['socket'])){ return 'Socket file not writeable.'; }
-  return 'OK';
-}
 
 function list_jails()
 { global $f2b; $jails=array();
-  $erg=@exec('fail2ban-client status | grep "Jail list:" | awk -F ":" \'{print $2}\' | awk \'{$1=$1;print}\'');
+  $erg=@exec('sudo /usr/bin/fail2ban-client status | grep "Jail list:" | awk -F ":" \'{print $2}\' | awk \'{$1=$1;print}\'');
   $erg=explode(",",$erg); foreach($erg as $i=>$j){ $jails[trim($j)]=false; }
   ksort($jails); return $jails;
 }
 
 function jail_info($jail)
 { global $f2b; $info=array();
-  $erg=@exec('fail2ban-client get '.escapeshellarg($jail).' findtime ');
+  $erg=@exec('sudo /usr/bin/fail2ban-client get '.escapeshellarg($jail).' findtime ');
   if(is_numeric($erg)){ $info['findtime']='findtime: '.$erg; }
-  $erg=@exec('fail2ban-client get '.escapeshellarg($jail).' bantime ');
+  $erg=@exec('sudo /usr/bin/fail2ban-client get '.escapeshellarg($jail).' bantime ');
   if(is_numeric($erg)){ $info['bantime']='bantime: '.$erg; }
-  $erg=@exec('fail2ban-client get '.escapeshellarg($jail).' maxretry ');
+  $erg=@exec('sudo /usr/bin/fail2ban-client get '.escapeshellarg($jail).' maxretry ');
   if(is_numeric($erg)){ $info['maxretry']='maxretry: '.$erg; }
   return $info;
 }
 
 function list_banned($jail)
 { global $f2b; $banned=array();
-  $erg=@exec('fail2ban-client status '.$jail.' | grep "IP list:" | awk -F ":" \'{print$2}\' | awk \'{$1=$1;print}\'');
+  $erg=@exec('sudo /usr/bin/fail2ban-client status '.$jail.' | grep "IP list:" | awk -F "list:" \'{print$2}\' | awk \'{$1=$1;print}\'');
   if($erg!='')
   { $banned=explode(" ",$erg);
     if($f2b['usedns']===true)
@@ -44,17 +38,17 @@ function list_banned($jail)
 }
 
 function ban_ip($jail,$ip)
-{ if($jail==''){ return 'no jail selected'; }
-  elseif(!filter_var($ip,FILTER_VALIDATE_IP)) { return 'no valid ip address'; }
-  $erg=@exec('fail2ban-client set '.escapeshellarg($jail).' banip '.escapeshellarg($ip));
-  if($erg!=$ip){ return 'could not ban this ip'; }
+{ if($jail==''){ return 'nojailselected';  }
+  elseif(!filter_var($ip,FILTER_VALIDATE_IP)) { return 'novalidipaddress'; }
+  $erg=@exec('sudo /usr/bin/fail2ban-client  set '.escapeshellarg($jail).' banip '.escapeshellarg($ip));
+  if($erg!=$ip){ return 'couldnotbanthisip'; }
   return 'OK';
 }
 
 function unban_ip($jail,$ip)
 { if($jail==''){ return 'no jail selected'; }
   elseif(!filter_var($ip,FILTER_VALIDATE_IP)) { return 'no valid ip address'; }
-  $erg=@exec('fail2ban-client set '.escapeshellarg($jail).' unbanip '.escapeshellarg($ip));
+  $erg=@exec('sudo /usr/bin/fail2ban-client  set '.escapeshellarg($jail).' unbanip '.escapeshellarg($ip));
   if($erg!=$ip){ return 'could not unban this ip'; }
   return 'OK';
 }
---
+++ language/en.php
@@ -0,0 +1,31 @@
+<?php
+
+$Refresh = "Refresh";
+
+$BannedclientsperJail = "Banned clients per Jail";
+
+$ManuallyaddbannedclienttoJail =  "Manually add banned client to Jail";
+
+$select = "select";
+
+$BanIP =  "Ban IP";
+
+$nobannedclients = "no banned clients";
+
+$UnbanIP = "Unban IP";
+
+$nojailselected = "no jail selected";
+
+$ipsuccessfullybanned = "ip successfully banned";
+
+$ipsuccessfullyunbanned = "ip successfully unbanned";
+
+$novalidipaddress = "no valid ip address";
+
+$couldnotbanthisip = "could not ban this ip";
+
+$version = "Version";
+
+$serviceerror = "Is fail2ban service active? Have you configured sudo?";
+
+?>
